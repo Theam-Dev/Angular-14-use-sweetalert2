406eeebeb79c0e16d925f20c80ac4575bbd3fb43
Pin Phearak <phearakroun@gmail.com>
2022-10-08 15:58:37 +0700

    Angular 14 sweetalert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-sweetalert2';
+  simpleAlert(){  
+    Swal.fire('Hello Angular');  
+  }  
+    
+  successAlert(){  
+    Swal.fire('Thank you...', 'You submitted succesfully!', 'success')  
+  }  
+  erroAlert()  
+  {  
+    Swal.fire({  
+      icon: 'error',  
+      title: 'Oops...',  
+      text: 'Something went wrong!',  
+      footer: '<a href>Why do I have this issue?</a>'  
+    })  
+  }  
+  confirmBoxAlert(){  
+    Swal.fire({  
+      title: 'Are you sure want to remove?',  
+      text: 'You will not be able to recover this file!',  
+      icon: 'warning',  
+      showCancelButton: true,  
+      confirmButtonText: 'Yes, delete it!',  
+      cancelButtonText: 'No, keep it'  
+    }).then((result) => {  
+      if (result.value) {  
+        Swal.fire(  
+          'Deleted!',  
+          'Your imaginary file has been deleted.',  
+          'success'  
+        )  
+      } else if (result.dismiss === Swal.DismissReason.cancel) {  
+        Swal.fire(  
+          'Cancelled',  
+          'Your imaginary file is safe :)',  
+          'error'  
+        )  
+      }  
+    })  
+  } 
 }
